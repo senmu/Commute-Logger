127a4a78d9156e601bf3f941e782ca04ca95e61f
Sam Lu <sam@gosammy.com>
2011-09-26 11:05:12 -0600

    Initial Commit

---
+++ Commute Tracker/Commute_TrackerAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  Commute_TrackerAppDelegate.h
+//  Commute Tracker
+//
+//  Created by Sam Lu on 11-09-26.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Commute_TrackerAppDelegate : NSObject <UIApplicationDelegate, UITabBarControllerDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
+
+@end
---
+++ Commute Tracker/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  Commute Tracker
+//
+//  Created by Sam Lu on 11-09-26.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
